701a5d73903fdb27cf1f73503bc0f406c561b3fe
ProfCrazy <ST-DDT@gmx.de>
2012-05-14 17:34:15 +0200

    CrazyLogin: prevents vehicle destruction, when not logged in

--- src/de/st_ddt/crazylogin/CrazyLoginVehicleListener.java
+++ src/de/st_ddt/crazylogin/CrazyLoginVehicleListener.java
@@ -3,6 +3,8 @@ package de.st_ddt.crazylogin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
 
 public class CrazyLoginVehicleListener implements Listener
@@ -10,21 +12,45 @@ public class CrazyLoginVehicleListener implements Listener
 
 	private final CrazyLogin plugin;
 
-	public CrazyLoginVehicleListener(CrazyLogin plugin)
+	public CrazyLoginVehicleListener(final CrazyLogin plugin)
 	{
 		super();
 		this.plugin = plugin;
 	}
 
 	@EventHandler
-	public void VehicleEnter(VehicleEnterEvent event)
+	public void VehicleEnter(final VehicleEnterEvent event)
 	{
 		if (!(event.getVehicle().getPassenger() instanceof Player))
 			return;
-		Player player = (Player) event.getVehicle().getPassenger();
+		final Player player = (Player) event.getVehicle().getPassenger();
 		if (plugin.isLoggedIn(player))
 			return;
 		event.setCancelled(true);
-		plugin.sendLocaleMessage("LOGIN.REQUEST", player);
+		plugin.requestLogin(player);
+	}
+
+	@EventHandler
+	public void VehicleDamage(final VehicleDamageEvent event)
+	{
+		if (!(event.getAttacker() instanceof Player))
+			return;
+		final Player player = (Player) event.getAttacker();
+		if (plugin.isLoggedIn(player))
+			return;
+		event.setCancelled(true);
+		plugin.requestLogin(player);
+	}
+
+	@EventHandler
+	public void VehicleDestroy(final VehicleDestroyEvent event)
+	{
+		if (!(event.getAttacker() instanceof Player))
+			return;
+		final Player player = (Player) event.getAttacker();
+		if (plugin.isLoggedIn(player))
+			return;
+		event.setCancelled(true);
+		plugin.requestLogin(player);
 	}
 }
