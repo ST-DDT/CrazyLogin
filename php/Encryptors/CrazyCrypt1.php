88e8a153c7be6e9c50b441c6150a9215087d4e46
ST-DDT <ST-DDT@gmx.de>
2012-11-06 17:20:25 +0100

    CrazyLogin: uploaded PHP version of CrazyCrpyt1

---
+++ php/Encryptors/CrazyCrypt1.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+function CrazyCrypt1($name,$password)
+{
+$text = "ÜÄaeut//&/=I " . $password . "7421€547" . $name . "__+IÄIH§%NK " . $password;
+$t1=unpack("H*",$text);
+$t2=substr($t1[1],0,strlen($text)*2-12);
+$t3=pack("H*",$t2);
+$hash=hash("sha512",$t3);
+return $hash;
+}
+
+?>
