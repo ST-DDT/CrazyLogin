82b1184de88c6644d4881cbdf6d1b9e46d0b0781
ST-DDT <ST-DDT@gmx.de>
2012-11-07 18:44:18 +0100

    CrazyLogin: fixed php version of CrazyCrypt when using special chars

--- php/Encryptors/CrazyCrypt1.php
+++ php/Encryptors/CrazyCrypt1.php
@@ -4,7 +4,7 @@ function CrazyCrypt1($name,$password)
 {
 $text = "ÜÄaeut//&/=I " . $password . "7421€547" . $name . "__+IÄIH§%NK " . $password;
 $t1=unpack("H*",$text);
-$t2=substr($t1[1],0,strlen($text)*2-12);
+$t2=substr($t1[1],0,mb_strlen($text,'UTF-8')*2);
 $t3=pack("H*",$t2);
 $hash=hash("sha512",$t3);
 return $hash;
