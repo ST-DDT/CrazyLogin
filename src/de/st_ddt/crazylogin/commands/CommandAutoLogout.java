b23495ea22efea3475f10b779533887e86ba6254
ST-DDT <ST-DDT@gmx.de>
2013-02-22 18:23:13 +0100

    CrazyLogin: added message to AutoLogoutCommand

--- src/de/st_ddt/crazylogin/commands/CommandAutoLogout.java
+++ src/de/st_ddt/crazylogin/commands/CommandAutoLogout.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import de.st_ddt.crazylogin.CrazyLogin;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandExecutorException;
 import de.st_ddt.crazyplugin.exceptions.CrazyException;
+import de.st_ddt.crazyutil.locales.Localized;
 
 public class CommandAutoLogout extends CommandExecutor
 {
@@ -16,11 +17,13 @@ public class CommandAutoLogout extends CommandExecutor
 	}
 
 	@Override
+	@Localized("CRAZYLOGIN.COMMAND.AUTOLOGOUT")
 	public void command(final CommandSender sender, final String[] args) throws CrazyException
 	{
 		if (!(sender instanceof Player))
 			throw new CrazyCommandExecutorException(false);
 		final Player player = (Player) sender;
 		plugin.getPlayerAutoLogouts().add(player);
+		plugin.sendLocaleMessage("COMMAND.AUTOLOGOUT", player);
 	}
 }
