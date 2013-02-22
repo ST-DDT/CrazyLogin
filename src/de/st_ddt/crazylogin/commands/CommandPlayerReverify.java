40fd62bcc1b81b1d2d4afbd681cc9f71ad00d78d
ST-DDT <ST-DDT@gmx.de>
2013-02-22 16:58:57 +0100

    CrazyLogin: send message to player who should reverify

--- src/de/st_ddt/crazylogin/commands/CommandPlayerReverify.java
+++ src/de/st_ddt/crazylogin/commands/CommandPlayerReverify.java
@@ -3,12 +3,13 @@ package de.st_ddt.crazylogin.commands;
 import java.util.List;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import de.st_ddt.crazylogin.CrazyLogin;
 import de.st_ddt.crazylogin.data.LoginPlayerData;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandNoSuchException;
-import de.st_ddt.crazyplugin.exceptions.CrazyCommandUsageException;
 import de.st_ddt.crazyplugin.exceptions.CrazyException;
+import de.st_ddt.crazyutil.ChatHelper;
 import de.st_ddt.crazyutil.locales.Localized;
 import de.st_ddt.crazyutil.modules.permissions.PermissionModule;
 import de.st_ddt.crazyutil.paramitrisable.PlayerDataParamitrisable;
@@ -25,9 +26,7 @@ public class CommandPlayerReverify extends CommandExecutor
 	@Localized("CRAZYLOGIN.COMMAND.PLAYER.REVERIFY.SUCCESS $Name$")
 	public void command(final CommandSender sender, final String[] args) throws CrazyException
 	{
-		if (args.length != 1)
-			throw new CrazyCommandUsageException("<Player>");
-		final String arg = args[0];
+		final String arg = ChatHelper.listingString(" ", args);
 		if (arg.equals("*"))
 		{
 			for (final LoginPlayerData data : plugin.getPlayerData())
@@ -40,6 +39,9 @@ public class CommandPlayerReverify extends CommandExecutor
 			if (data == null)
 				throw new CrazyCommandNoSuchException("Player (with Account)", arg);
 			data.setLoggedIn(false);
+			final Player player = data.getPlayer();
+			if (player != null)
+				plugin.requestLogin(player);
 			plugin.sendLocaleMessage("COMMAND.PLAYER.REVERIFY.SUCCESS", sender, data.getName());
 		}
 	}
