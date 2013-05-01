05011be240a521895b200e9b635ea818fd239788
ST-DDT <ST-DDT@gmx.de>
2013-05-01 03:56:11 +0200

    CrazyLogin: added missing import

--- src/de/st_ddt/crazylogin/listener/PlayerListener.java
+++ src/de/st_ddt/crazylogin/listener/PlayerListener.java
@@ -2,6 +2,7 @@ package de.st_ddt.crazylogin.listener;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
