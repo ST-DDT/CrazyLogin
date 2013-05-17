22fc47ad2433583b9a41d0fc227fd750d9c8f4af
ST-DDT <ST-DDT@gmx.de>
2013-05-17 22:50:18 +0200

    CrazyLogin: updated to latest CrazyCore version

--- src/de/st_ddt/crazylogin/commands/CommandExecutor.java
+++ src/de/st_ddt/crazylogin/commands/CommandExecutor.java
@@ -2,9 +2,9 @@ package de.st_ddt.crazylogin.commands;
 
 import de.st_ddt.crazylogin.CrazyLogin;
 import de.st_ddt.crazylogin.data.LoginData;
-import de.st_ddt.crazyplugin.commands.CrazyPlayerDataCommandExecutor;
+import de.st_ddt.crazyplugin.commands.CrazyPlayerDataPluginCommandExecutor;
 
-public abstract class CommandExecutor extends CrazyPlayerDataCommandExecutor<LoginData, CrazyLogin>
+public abstract class CommandExecutor extends CrazyPlayerDataPluginCommandExecutor<LoginData, CrazyLogin> implements CommandExecutorInterface<CrazyLogin>
 {
 
 	public CommandExecutor(final CrazyLogin plugin)
